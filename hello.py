4de7665fe4a7250390497589c0477f8ec90bb8a3
juanargoti <juanargoti2015@hotmail.com>
2019-08-05 21:49:52 -0500

    upload basic calc

---
+++ hello.py
@@ -0,0 +1,12 @@
+# THIS IS MY FRIST PYTHON PROGRAM 
+# DEVELOPER: JUAN ARGOTI 
+a = 5
+b= 10
+
+
+	PRINT (" ::: BASIC CALC:::")
+	
+	print ("the add is : ",a +b )
+	print ("the subs is : ",a -b )
+	print ("the div is : ",a /b )
+	print ("the mult is : ",a *b )
